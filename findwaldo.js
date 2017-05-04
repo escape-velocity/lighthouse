1b7b52f30d23ae34b737ec013a923c306543c2a6
Bill <networker747@gmail.com>
2017-05-04 18:30:46 +0000

    callbacks practice

---
+++ findwaldo.js
@@ -0,0 +1,17 @@
+
+function findWaldo(arr, found) {
+  for (var i = 0; i <arr.length; i++) {
+      if (arr[i] === "Waldo") {
+        found();   //execute callback
+     }
+  }
+
+}
+
+
+function actionWhenFound() {
+  console.log("Found Him!")
+}
+
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
