a9d764f80da9184d46f47db2fe60555d2a72299d
Bill <networker747@gmail.com>
2017-05-05 21:56:56 +0000

    using import/export

---
+++ LL-is-fun.js
@@ -0,0 +1,10 @@
+
+module.exports =  {
+  x: "Hastings st.",
+  //x is delivered into the console.log below
+  y: function() {
+    console.log("this is the location of LL " + this.x);
+  }
+  //The combo of the two strings above get returned to the import_export file
+};
+
