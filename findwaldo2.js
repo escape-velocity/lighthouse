283912c84e4fda4a0ea21932bb93e531dbdbc3ca
Bill <networker747@gmail.com>
2017-06-01 18:46:55 +0000

    used callback and forEach loop

---
+++ findwaldo2.js
@@ -0,0 +1,15 @@
+
+
+function findWaldo(arr, found) {
+  arr.forEach(function (name, index) {
+    if (name === "Waldo") {
+      found(name, index);   // execute callback
+    }
+  })
+}
+
+function actionWhenFound(name, index) {
+  console.log("Found" + name + " at index " + index + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
