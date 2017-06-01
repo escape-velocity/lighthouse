36889743ad4434f378c33296f1d16d2c17bec5d3
Bill <networker747@gmail.com>
2017-06-01 22:31:55 +0000

    rewrite of nested function

--- makeloadeddie2.js
+++ makeloadeddie2.js
@@ -1,15 +1,12 @@
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  var id = 0;
+  for (var i = 0; i < list.length; i++) {
+  }
+
+  var nextNumber = 0
 
   return function() {
-    var number = list[id];
-    if (id >= list.length - 1) {
-      id = 0;
-    } else {
-      id++;
-    }
-    return number;
+    return list[nextNumber++];
   }
 }
 
